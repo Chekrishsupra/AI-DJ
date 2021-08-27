f464ecfae16a9342d727eaaab159c7d8a1c944f5
Chekrishsuper <78082844+Chekrishsupra@users.noreply.github.com>
2021-08-27 16:49:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,80 @@
+song = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+scoreleftwrist = 0;
+scorerightwrist = 0;
+function preload() {
+    song = loadSound("music.mp3");
+}
+function setup() {
+    canvas = createCanvas(500,400);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    posenet = ml5.poseNet(video,modelLoaded);
+    posenet.on("pose",gotposes);
+}
+function gotposes(results) {
+    if(results.length > 0) {
+        console.log(results);
+        scoreleftwrist = results[0].pose.keypoints[9].score;
+        scorerightwrist = results[0].pose.keypoints[10].score;
+        console.log("Score of left wrist= " + scoreleftwrist);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("Left wrist x = " + leftWristX + ", Left wrist y = " + leftWristY);
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("Right wrist x = " + rightWristX + ", Right wrist y = " + rightWristY);
+    }
+}
+
+function draw() {
+    image(video,0,0,500,400);
+    fill('#ff0400');
+    stroke('#ff0400');
+    if(scorerightwrist>0.2) {
+
+    circle(rightWristX,rightWristY,20);
+    if(rightWristY>0 && rightWristY<=100) {
+        document.getElementById("speed").innerHTML = "Speed= 0.5x";
+        song.rate(0.5);
+    }
+    else if(rightWristY>100 && rightWristY<=200) {
+        document.getElementById("speed").innerHTML = 'Speed= 1x';
+        song.rate(1);
+    }
+    else if(rightWristY>200 && rightWristY<=300) {
+        document.getElementById("speed").innerHTML = "Speed= 1.5x";
+        song.rate(1.5);
+    }
+    else if(rightWristY>300 && rightWristY<=400) {
+        document.getElementById("speed").innerHTML = "Speed= 2x";
+        song.rate(2);
+    }
+    else if(rightWristY>400) {
+      document.getElementById("speed").innerHTML = "Speed= 2.5x";
+      song.rate(2.5);
+    }
+}
+    if(scoreleftwrist > 0.2) {
+        circle(leftWristX,leftWristY,20);
+    InnumberleftWristY = Number(leftWristY);
+    removedecimals = floor(InnumberleftWristY);
+    volume = removedecimals/500;
+    document.getElementById("volume"). innerHTML = "Volume= " + volume;
+    song.setVolume(volume);
+}
+    }
+ 
+
+function play() {
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+function modelLoaded() {
+    console.log("Posenet is initialized");
+}
